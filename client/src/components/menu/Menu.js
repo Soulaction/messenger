10968a1888fd069ab1fbd65598cfd6a68d68ae0e
Cyber Cat <ilya-novyy@mail.ru>
2022-09-07 21:12:54 +0300

    my branch

--- client/src/components/menu/Menu.js
+++ client/src/components/menu/Menu.js
@@ -1,18 +1,14 @@
 import { useSelector } from "react-redux";
-import s from "./Menu.module.css"
-
+import s from "./Menu.module.css";
 
 const Menu = () => {
-
-    const isAuth = useSelector(state => state.user.isAuth)
-    console.log(isAuth)
-    return (
-        <div className={isAuth ? s.menu : s.no_menu}>
-            <div class={s.rotate_shadows}>
-                Меню
-            </div>
-        </div>)
-
-}
+  const isAuth = useSelector((state) => state.user.isAuth);
+  console.log(isAuth);
+  return (
+    <div className={isAuth ? s.menu : s.no_menu}>
+      <div class={s.rotate_shadows}>Меню The Best</div>
+    </div>
+  );
+};
 
 export default Menu;
--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -4,71 +4,79 @@ import { Link, useNavigate } from "react-router-dom";
 import { authorization } from "../../http/userAPI";
 import { setAuthUser, setUser } from "../../store/user/action";
 import { MAIN_ROUT, REGISTRATION_ROUT } from "../../untils/ConstRout";
-import s from "./Auth.module.css"
+import s from "./Auth.module.css";
 
 const Auth = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const email = useRef("");
+  const password = useRef("");
 
-    const email = useRef('')
-    const password = useRef('')
+  const [checkEmail, setCheckEmail] = useState(false);
+  const [checkPassword, setCheckPassword] = useState(false);
+  const [warn, setWarn] = useState("");
 
-    const [checkEmail, setCheckEmail] = useState(false)
-    const [checkPassword, setCheckPassword] = useState(false)
-    const [warn, setWarn] = useState('')
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const auth = async () => {
+    setWarn("");
 
-    const auth = async () => {
-
-        setWarn('')
-
-        if (!email.current.value) {
-            setCheckEmail(true)
-        } else {
-            setCheckEmail(false)
-        }
-        if (!password.current.value) {
-            setCheckPassword(true)
-        } else {
-            setCheckPassword(false)
-        }
-        if (email.current.value && password.current.value) {
-            
-            try {
-                let data = await authorization(email.current.value, password.current.value)
-
-                data = {...data, isAuth: true}
-                dispatch(setUser(data))
-                navigate(MAIN_ROUT)
-
-            } catch (e) {
-                setWarn(e.response.data)
-                email.current.value = ''
-                password.current.value = ''
-            }
-        }
+    if (!email.current.value) {
+      setCheckEmail(true);
+    } else {
+      setCheckEmail(false);
     }
+    if (!password.current.value) {
+      setCheckPassword(true);
+    } else {
+      setCheckPassword(false);
+    }
+    if (email.current.value && password.current.value) {
+      try {
+        let data = await authorization(
+          email.current.value,
+          password.current.value
+        );
 
-    const user = useSelector(state => state.user)
-    return (
-        <div className={s.auth}>
-            <div className={s.form}>
-                <h1>Авторизация</h1>
-                <input ref={email} className={checkEmail ? s.warning : ''} placeholder="Введите email"></input>
-                <input ref={password} className={checkPassword ? s.warning : ''} type="password" placeholder="Введите пароль"></input>
-                <h4 className={warn ? s.warn_mes_y : s.warn_mes_n}>{warn}</h4>
-                <div className={s.redirect}>
-                    <div className={s.auth}>
-                        <Link to={REGISTRATION_ROUT}>Регистрация</Link>
-                    </div>
-                    <div className={s.registration}>
-                        <button onClick={auth}>Войти</button>
-                    </div>
-                </div>
-            </div>
+        data = { ...data, isAuth: true };
+        dispatch(setUser(data));
+        navigate(MAIN_ROUT);
+      } catch (e) {
+        setWarn(e.response.data);
+        email.current.value = "";
+        password.current.value = "";
+      }
+    }
+  };
+
+  const user = useSelector((state) => state.user);
+  return (
+    <div className={s.auth}>
+      <div className={s.form}>
+        <h1>Авторизация</h1>
+        <input
+          ref={email}
+          className={checkEmail ? s.warning : ""}
+          placeholder="Введите email"
+        ></input>
+        <input
+          ref={password}
+          className={checkPassword ? s.warning : ""}
+          type="password"
+          placeholder="Введите пароль"
+        ></input>
+        <h4 className={warn ? s.warn_mes_y : s.warn_mes_n}>{warn}</h4>
+        <div className={s.redirect}>
+          <div className={s.auth}>
+            <Link to={REGISTRATION_ROUT}>Регистрация !!!</Link>
+          </div>
+          <div className={s.registration}>
+            <button onClick={auth}>Войти</button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
